4f5d0d7c184494531c4d72ad9817973d3fb463ab
kiran-bsv <boddepallisai@iitbhilai.ac.in>
2024-07-27 18:42:30 +0530

    ..

--- __init__.py
+++ __init__.py
@@ -40,4 +40,5 @@ def do_this():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
